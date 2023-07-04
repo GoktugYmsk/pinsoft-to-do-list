c1d359168947bfd1574229cdfae16db208ab8a52
GoktugYmsk <gogtug033@outlook.com>
2023-07-04 09:38:04 +0300

    task bug added from input has been resolved

--- src/compoents/content/midddleContent/index.jsx
+++ src/compoents/content/midddleContent/index.jsx
@@ -5,36 +5,52 @@ import './index.scss';
 import { useSelector } from 'react-redux';
 
 function MiddleContent({ selectedTasks, setDoneTasks }) {
-  const addTask = useSelector((state) => state.addTodo.addTask);
+    const addTask = useSelector((state) => state.addTodo.addTask);
 
-  const handleTaskClick = (index) => {
-    const optionTask = addTask[index];
-    setDoneTasks((prevDoneTasks) => [...prevDoneTasks, optionTask]);
-  };
+    const handleTaskClick = (index) => {
+        const optionTask = addTask[index];
+        setDoneTasks((prevDoneTasks) => [...prevDoneTasks, optionTask]);
+    };
 
-  return (
-    <div className='middleconent'>
-      <h2>DOING</h2>
-      <div className='middlecontent__list'>
-        {selectedTasks.map((index) => (
-          addTask[index] && (
-            <div className='middlecontent__list__todoCheck' key={index}>
-              <ul>
-                <div className="middleContent__box ">
-                  <FaTrashAlt className='middleContent__box-icon__left' />
-                  <li>{addTask[index]}</li>
-                  <BsCheckCircleFill
-                    onClick={() => handleTaskClick(index)}
-                    className='middleContent__box-icon__right'
-                  />
-                </div>
-              </ul>
+    return (
+        <div className='middleconent'>
+            <h2>DOING</h2>
+            <div className='middlecontent__list'>
+                {selectedTasks.map((task, index) => (
+                    task && (
+                        <div className='middlecontent__list__todoCheck' key={index}>
+                            <ul>
+                                <div className="middleContent__box ">
+                                    <FaTrashAlt className='middleContent__box-icon__left' />
+                                    <li>{task}</li>
+                                    <BsCheckCircleFill
+                                        onClick={() => handleTaskClick(index)}
+                                        className='middleContent__box-icon__right'
+                                    />
+                                </div>
+                            </ul>
+                        </div>
+                    )
+                ))}
             </div>
-          )
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default MiddleContent;
+
+
+
+// const handleTaskClick = (index) => {
+//     if (selectedTasks.includes(index)) {
+//         setSelectedTasks(selectedTasks.filter((item) => item !== index));
+//     } else {
+//         setSelectedTasks([...selectedTasks, index]);
+//     }
+// };
+
+
+// const handleDeleteClick = (index) => {
+//     const updatedAddTask = addTask.filter((item, idx) => idx !== index);
+//     dispatch(setAddTasks(updatedAddTask));
+// };
