8e8112f341adaaa83796dc42ed2415d6f1a10796
Burcu Taş <burcutas636@gmail.com>
2023-07-04 13:45:02 +0300

    adding task to database

--- src/compoents/footer/index.jsx
+++ src/compoents/footer/index.jsx
@@ -2,15 +2,23 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setAddTasks } from '../configure';
 
+import {app,database} from "../../firebase";
+import { collection, addDoc } from 'firebase/firestore';
+
 import './index.scss';
 
 function Footer() {
+    const [data , setData] = useState({});
+    const collectionRef = collection(database, "todo");
+
     const [input, setInput] = useState('');
     const dispatch = useDispatch();
     const addTask = useSelector((state) => state.addTodo.addTask);
 
     const handleChange = (event) => {
         setInput(event.target.value);
+        let newInput = {[event.target.name]: event.target.value};
+        setData({ ...data, ...newInput});
     };
 
     const handleSubmit = () => {
@@ -18,12 +26,22 @@ function Footer() {
             const updatedTasks = [...addTask, input];
             dispatch(setAddTasks(updatedTasks));
             setInput('');
+
+            addDoc(collectionRef, {
+                task: data.task
+            })
+              .then(() => {
+                alert("Data Added!");
+              })
+              .catch((err) => {
+                alert(err.message);
+              })
         }
     };
 
     return (
         <div className='footer-container'>
-            <input onChange={handleChange} placeholder='New Item' value={input} />
+            <input name='task' onChange={handleChange} placeholder='New Item' value={input} />
             <button onClick={handleSubmit}>Add Task</button>
         </div>
     );
