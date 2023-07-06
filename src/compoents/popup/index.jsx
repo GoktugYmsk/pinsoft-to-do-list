ede96ffdac198b2c745e712a78b93481e8e01c68
GoktugYmsk <gogtug033@outlook.com>
2023-07-06 22:28:32 +0300

    firebase added

--- src/compoents/popup/index.jsx
+++ src/compoents/popup/index.jsx
@@ -1,21 +1,16 @@
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { setPopupModal, setAddTasks, setDoingTask } from '../configure';
+import { app, db } from '../../firebase';
+import { collection, addDoc, doc, updateDoc } from 'firebase/firestore';
+import './index.scss';
 
-import {app,database} from "../../firebase";
-
-import { collection, addDoc } from 'firebase/firestore';
-
-import './index.scss'
-
-function Popup({setSelectedTasks}) {
-    const [data , setData] = useState({});
-
+function Popup({ setSelectedTasks }) {
+    const [data, setData] = useState({});
     const [input, setInput] = useState('');
+
     const active = useSelector((state) => state.darkActive.active);
-    const popupModel = useSelector((state) => state.modal.popupModal);
+    const popupModal = useSelector((state) => state.modal.popupModal);
     const addTask = useSelector((state) => state.addTodo.addTask);
     const doingTask = useSelector((state) => state.doing.doingTask);
 
@@ -23,37 +18,52 @@ function Popup({setSelectedTasks}) {
 
     const handleChange = (event) => {
         setInput(event.target.value);
-        let newInput = {[event.target.name]: event.target.value};
-        setData({ ...data, ...newInput});
+        let newInput = { [event.target.name]: event.target.value };
+        setData({ ...data, ...newInput });
     };
-
-    const handleToDoClick = () => {
+    
+    const handleToDoClick = async () => {
         if (input.trim() !== '') {
-            const updatedTasks = [...addTask, input];
+            const updatedTasks = [...addTask, { task: input, status: 0 }];
             dispatch(setAddTasks(updatedTasks));
             setInput('');
-            console.log('Todo',updatedTasks)
 
-            const collectionRef = collection(database, "todo list"); 
-            addDoc(collectionRef, {
-                task: data.task
-            })
+            try {
+                const collectionRef = collection(db, 'todo list');
+                const docRef = await addDoc(collectionRef, { task: input, status: 0 });
+                console.log('Veri Firebase\'e gönderildi. Doküman ID:', docRef.id);
+
+                // Firestore'da güncelleme yapmak için kullanılabilir
+                const docId = docRef.id;
+                const todoDocRef = doc(db, 'todo list', docId);
+                await updateDoc(todoDocRef, { status: 0 });
+
+                // Örneğin, todo listesindeki verinin durumunu güncellemek için yukarıdaki gibi updateDoc kullanabilirsiniz
+                // İlgili alanları kendi gereksinimlerinize göre ayarlayın
+
+            } catch (error) {
+                console.error('Veri gönderme hatası:', error);
+            }
         }
-    }
+    };
 
-    const handleDoingClick = () => {
+
+    const handleDoingClick = async () => {
         if (input.trim() !== '') {
             const updatedDoingTasks = [...doingTask, input];
-            dispatch(setDoingTask(updatedDoingTasks))
-            setSelectedTasks(updatedDoingTasks)
+            dispatch(setDoingTask(updatedDoingTasks));
+            setSelectedTasks(updatedDoingTasks);
             setInput('');
 
-            const collectionRef = collection(database, "doing list"); 
-            addDoc(collectionRef, {
-                task: data.task
-            })
+            try {
+                const collectionRef = collection(db, 'doing list');
+                const docRef = await addDoc(collectionRef, { task: input });
+                console.log('Veri Firebase\'e gönderildi. Doküman ID:', docRef.id);
+            } catch (error) {
+                console.error('Veri gönderme hatası:', error);
+            }
         }
-    }
+    };
 
     const handleHideModal = () => {
         dispatch(setPopupModal(false));
@@ -73,25 +83,24 @@ function Popup({setSelectedTasks}) {
         };
     }, []);
 
-
     return (
         <>
-            {popupModel && (
+            {popupModal && (
                 <div className={`modal-container  ${active ? 'modal-container-active' : 'modal-container'}`}>
                     <div className="modal-content">
                         <span className="modal-close" onClick={handleHideModal}>
                             &times;
                         </span>
-                        <input name='task' onChange={handleChange} value={input} placeholder='New Item' />
-                        <div className='modal-content__options' >
-                            <button onClick={handleToDoClick} >ToDo </button>
-                            <button onClick={handleDoingClick}> Doing</button>
+                        <input name="task" onChange={handleChange} value={input} placeholder="New Item" />
+                        <div className="modal-content__options">
+                            <button onClick={handleToDoClick}>ToDo</button>
+                            <button onClick={handleDoingClick}>Doing</button>
                         </div>
                     </div>
                 </div>
             )}
         </>
-    )
+    );
 }
 
-export default Popup
+export default Popup;
