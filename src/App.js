b6064ed333b1c2661e3c2405aa1799077329ea9f
Melihcan Şimşek <melihsimsek02@gmail.com>
2023-07-06 23:11:06 +0300

    body background color fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import {  useSelector } from 'react-redux';
 import Login from "./compoents/content/login";
 import Components from "./compoents";
@@ -9,6 +9,22 @@ import "./App.css";
 
 function App() {
   const active = useSelector((state) => state.darkActive.active);
+
+  useEffect(() => {
+      if (active) {
+        document.body.style.backgroundColor = '#dfe2e7 '; 
+      } else {
+        document.body.style.backgroundColor = '#242424';
+      }
+  
+      
+      return () => {
+        document.body.style.backgroundColor = null;
+        document.body.style.color = null;
+      };
+    }, [active]);
+
+
   return (
     <div className={`App  ${active ? 'app-active ' : 'App'}`}>
 
