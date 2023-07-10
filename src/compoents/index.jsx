13c40b749a6a81b43d9fbd9e4eaefc1a62f6d828
GoktugYmsk <gogtug033@outlook.com>
2023-07-10 13:09:30 +0300

    logout popup added

--- src/compoents/index.jsx
+++ src/compoents/index.jsx
@@ -6,21 +6,31 @@ import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
 import { FiLogOut } from 'react-icons/fi';
 import Form from 'react-bootstrap/Form';
 import { setActive } from './configure';
+import Toast from 'react-bootstrap/Toast';
+
 
 import Content from '../compoents/content';
 import Footer from '../compoents/footer';
 import Popup from '../compoents/popup';
 
 function Components() {
+
+    const [popup, setPopup] = useState(false)
+    const [selectedTasks, setSelectedTasks] = useState([]);
+    const navigate = useNavigate();
+
     const active = useSelector((state) => state.darkActive.active);
     const popupModel = useSelector((state) => state.modal.popupModal);
-    const navigate = useNavigate();
 
     const dispatch = useDispatch();
 
     const switchClick = () => {
         dispatch(setActive(!active));
     };
+
+    const handleClosePage = () => {
+        setPopup(true)
+    }
 
     const handleLogout = async () => {
         const auth = getAuth();
@@ -32,7 +42,6 @@ function Components() {
         }
     };
 
-    const [selectedTasks, setSelectedTasks] = useState([]);
 
     useEffect(() => {
         const auth = getAuth();
@@ -52,9 +61,26 @@ function Components() {
             <Helmet>
                 <title>Pinsoft To-Do-List</title>
             </Helmet>
+            <div className="toast-container">
+                <Toast className='toast-container__box' onClose={() => setPopup(false)} show={popup} delay={3000} autohide>
+                    <Toast.Header className='toast-container__header' >
+                        <strong>Are you sure</strong>
+                    </Toast.Header>
+                    <Toast.Body>
+                        <div className="d-flex justify-content-end">
+                            <button className="btn btn-secondary mx-1" onClick={() => setPopup(false)}>
+                                Cancel
+                            </button>
+                            <button className="btn btn-primary mx-1" onClick={handleLogout}>
+                                Continue
+                            </button>
+                        </div>
+                    </Toast.Body>
+                </Toast>
+            </div>
             <div className={`components__icon ${popupModel ? 'components__icon__opacity' : ''}`}>
                 <Form.Check type="switch" id="custom-switch" className="custom-switch " onClick={switchClick} />
-                <FiLogOut className={`logout__switch ${!active ? 'logout__switch__acitive' : ''}`} onClick={handleLogout} />
+                <FiLogOut className={`logout__switch ${!active ? 'logout__switch__acitive' : ''}`} onClick={handleClosePage} />
             </div>
             <div className="row header-container mb-3">
                 <h1 id="header">To Do List</h1>
